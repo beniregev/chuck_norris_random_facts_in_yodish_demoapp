f411753259aada1cba858c9613f48b0316d14e9b
binyamin.regev <beniregev@gmail.com>
2019-09-12 19:24:26 +0300

    Initial Version

---
+++ src/main/java/com/chuck_norris_random_facts_in_yodish/ChuckNorrisRandomFactsInYodishApplication.java
@@ -0,0 +1,13 @@
+package com.chuck_norris_random_facts_in_yodish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChuckNorrisRandomFactsInYodishApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChuckNorrisRandomFactsInYodishApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chuck_norris_random_facts_in_yodish/ChuckNorrisRandomFactsInYodishApplicationTests.java
@@ -0,0 +1,16 @@
+package com.chuck_norris_random_facts_in_yodish;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ChuckNorrisRandomFactsInYodishApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
