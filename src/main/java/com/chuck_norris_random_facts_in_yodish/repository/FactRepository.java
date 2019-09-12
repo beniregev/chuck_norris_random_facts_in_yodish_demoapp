58f5504d7e1141b2c1b323d06cd8cf834c3a3208
binyamin.regev <beniregev@gmail.com>
2019-09-12 19:41:41 +0300

    Adding @Repository annotation

--- src/main/java/com/chuck_norris_random_facts_in_yodish/repository/FactRepository.java
+++ src/main/java/com/chuck_norris_random_facts_in_yodish/repository/FactRepository.java
@@ -2,10 +2,12 @@ package com.chuck_norris_random_facts_in_yodish.repository;
 
 import com.chuck_norris_random_facts_in_yodish.model.Fact;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@Repository
 public interface FactRepository extends MongoRepository<Fact, String> {
     List<Fact> findByCreatedOn(LocalDate createdOn);
 }
